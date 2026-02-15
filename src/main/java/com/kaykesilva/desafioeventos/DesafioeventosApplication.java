49534774c1ca24bc011c68971f75d7d3b6ebb337
Kayke Moreira <kayke.allogins@gmail.com>
2026-02-15 13:11:03 -0300

    Start Project

---
+++ src/main/java/com/kaykesilva/desafioeventos/DesafioeventosApplication.java
@@ -0,0 +1,13 @@
+package com.kaykesilva.desafioeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioeventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioeventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaykesilva/desafioeventos/DesafioeventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaykesilva.desafioeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioeventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
